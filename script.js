114507b97329907ac600e645f446a5934b52d5c7
Sabine Gruber <sab@vollbio.de>
2018-04-21 11:24:59 +0200

    add discount prices

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ var PriceTwo = 2360;
 var sumOne = (PriceOne + PriceTwo);
 document.write("<br>" + "If you want to buy a " + ProductOne + " and a " + ProductTwo + ", it costs " + sumOne + " EUR." + "<br>");
 
+var discountOne = (sumOne / 10);
+var discountSumOne = (sumOne - discountOne);
+document.write("<br>" + "If you want to buy a " + ProductOne + " and a " + ProductTwo + " as a GoldenCard holder, you will get a 10% discount: " + discountSumOne + "<br>");
+
 var ProductThree = "trip to Rome";
 var ProductFour = "trip to Lisbon";
 var PriceThree = 2178;
 var PriceFour = 2286;
 var sumTwo = (PriceThree + PriceFour);
-document.write("<br>" + "If you want to buy a " + ProductThree + " and a " + ProductFour + ", it costs " + sumTwo + " EUR.");
+document.write("<br><br>" + "If you want to buy a " + ProductThree + " and a " + ProductFour + ", it costs " + sumTwo + " EUR." + "<br>");
+
+var discountTwo = (sumTwo / 10);
+var discountSumTwo = (sumTwo - discountTwo);
+document.write("<br>" + "If you want to buy a " + ProductThree + " and a " + ProductFour + " as a GoldenCard holder, you will get a 10% discount: " + discountSumTwo + "<br>");
