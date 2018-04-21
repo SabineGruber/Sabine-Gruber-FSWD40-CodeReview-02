e557fb116c4af687c593f6ea8aa909e567a13e30
Sabine Gruber <sab@vollbio.de>
2018-04-21 11:41:53 +0200

    add paragraphs in js

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ var ProductTwo = "trip to London";
 var PriceOne = 1189;
 var PriceTwo = 2360;
 var sumOne = (PriceOne + PriceTwo);
-document.write("<br>" + "If you want to buy a " + ProductOne + " and a " + ProductTwo + ", it costs " + sumOne + " EUR." + "<br>");
+document.write("<br>" + "<p>" + "If you want to buy a " + "<b>" + ProductOne + "</b>" + " and a " + "<b>" + ProductTwo + "</b>" + ", it costs:" + "</p>" + "<p>" + sumOne + " EUR." + "</p>");
 
 var discountOne = (sumOne / 10);
 var discountSumOne = (sumOne - discountOne);
-document.write("<br>" + "If you want to buy a " + ProductOne + " and a " + ProductTwo + " as a GoldenCard holder, you will get a 10% discount: " + discountSumOne + "<br>");
+document.write("<br>" + "<p>" + "If you want to buy a " + "<b>" + ProductOne + "</b>" + " and a " + "<b>" + ProductTwo + "</b>" + " as a GoldenCard holder, you will get a 10% discount:" + "</p>" + "<p>" + discountSumOne + "</p>");
 
 var ProductThree = "trip to Rome";
 var ProductFour = "trip to Lisbon";
 var PriceThree = 2178;
 var PriceFour = 2286;
 var sumTwo = (PriceThree + PriceFour);
-document.write("<br><br>" + "If you want to buy a " + ProductThree + " and a " + ProductFour + ", it costs " + sumTwo + " EUR." + "<br>");
+document.write("<br>" + "<p>" + "If you want to buy a " + "<b>" + ProductThree + "</b>" + " and a " + "<b>" + ProductFour + "</b>" + ", it costs:" + "</p>" + "<p>" + sumTwo + " EUR." + "</p>");
 
 var discountTwo = (sumTwo / 10);
 var discountSumTwo = (sumTwo - discountTwo);
-document.write("<br>" + "If you want to buy a " + ProductThree + " and a " + ProductFour + " as a GoldenCard holder, you will get a 10% discount: " + discountSumTwo + "<br>");
+document.write("<br>" + "<p>" + "If you want to buy a " + "<b>" + ProductThree + "</b>" + " and a " + "<b>" + ProductFour + "</b>" + " as a GoldenCard holder, you will get a 10% discount:" + "</p>" + "<p>" + discountSumTwo + "</p>");
