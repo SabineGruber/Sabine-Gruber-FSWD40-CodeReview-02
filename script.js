2161690fac7b281b4f8820026fdbf665e9384608
Sabine Gruber <sab@vollbio.de>
2018-04-21 11:09:12 +0200

    add media screen

--- script.js
+++ script.js
@@ -2,5 +2,12 @@ var ProductOne = "trip to Amsterdam";
 var ProductTwo = "trip to London";
 var PriceOne = 1189;
 var PriceTwo = 2360;
-var sum = (PriceOne + PriceTwo);
-document.write("If you want to buy a " + ProductOne + " and a " + ProductTwo + ", it costs " + sum + " EUR.");
+var sumOne = (PriceOne + PriceTwo);
+document.write("<br>" + "If you want to buy a " + ProductOne + " and a " + ProductTwo + ", it costs " + sumOne + " EUR." + "<br>");
+
+var ProductThree = "trip to Rome";
+var ProductFour = "trip to Lisbon";
+var PriceThree = 2178;
+var PriceFour = 2286;
+var sumTwo = (PriceThree + PriceFour);
+document.write("<br>" + "If you want to buy a " + ProductThree + " and a " + ProductFour + ", it costs " + sumTwo + " EUR.");
